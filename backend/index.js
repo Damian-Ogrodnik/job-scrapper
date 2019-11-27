cc46d7a346c1144670965e00b58f9b04f0e1390e
dogrodnik <d.ogrodnik96@gmail.com>
2019-11-27 09:10:45 +0100

    Create separate file for app and server

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const config = require("./src/server/config");
+const app = require("./src/server/server");
+require("./src/server/database/database");
+
+const PORT = process.env.PORT || config.PORT;
+
+app.listen(PORT, () => {
+  console.log("Server is listening...");
+});
--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -1,16 +1,10 @@
 const cors = require("cors");
 const express = require("express");
-const config = require("./config");
 const route = require("./routes/route");
-require("./database/database");
 
 const app = express();
 
 app.use(cors());
 app.use("/", route);
 
-const PORT = process.env.PORT || config.PORT;
-
-app.listen(PORT, () => {
-  console.log("Server is listening...");
-});
+module.exports = app;
