2d484415d9f8e05cbbd8de1a082cf044f94aaaae
dogrodnik <d.ogrodnik96@gmail.com>
2019-12-03 08:33:07 +0100

    Add try/catch in offersController.js

--- backend/src/server/controllers/offersController.js
+++ backend/src/server/controllers/offersController.js
@@ -8,24 +8,30 @@ exports.checkOffers = async function(req, res) {
   category = category.toLowerCase();
 
   let jobOffer = await Search.findOne({ name: `${city}-${category}` });
-
-  if (jobOffer) {
-    let time = Date.now() - new Date(jobOffer.date);
-    let timeMinutes = Math.floor(time / 60000);
-    if (timeMinutes >= 0 && timeMinutes < 60) {
-      res.status(200).send(jobOffer.data);
+  try {
+    if (jobOffer) {
+      let time = Date.now() - new Date(jobOffer.date);
+      let timeMinutes = Math.floor(time / 60000);
+      if (timeMinutes >= 0 && timeMinutes < 60) {
+        res.status(200).send(jobOffer.data);
+      } else {
+        const response = await api.getAllOffers(city, category, pages);
+        await Search.findOneAndUpdate(
+          { name: `${city}-${category}` },
+          { data: response, date: Date.now() }
+        );
+        res.status(200).send(response);
+      }
     } else {
       const response = await api.getAllOffers(city, category, pages);
-      await Search.findOneAndUpdate(
-        { name: `${city}-${category}` },
-        { data: response, date: Date.now() }
-      );
+      const search = new Search({
+        name: `${city}-${category}`,
+        data: response
+      });
+      await search.save();
       res.status(200).send(response);
     }
-  } else {
-    const response = await api.getAllOffers(city, category, pages);
-    const search = new Search({ name: `${city}-${category}`, data: response });
-    await search.save();
-    res.status(200).send(response);
+  } catch (err) {
+    res.status(500).send("Internal server error...");
   }
 };
